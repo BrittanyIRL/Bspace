6e4ce20bd1b9f7a760b100af2fce252f5dcc8f80
Brittany Brassell <brittany.brassell@gmail.com>
2016-11-21 20:16:56 -0800

    migrating up a level, swapping out url configs

---
+++ _site/javascript/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	//home page 
+	$("#homeContainer > div").hover(function(){
+		$(this).find(".excerptHover").addClass("show");
+	},
+	function(){
+		$(this).find(".excerptHover").removeClass("show");
+	});
+
+
+
+
+	//FOOTER
+	// external link colors to match their site brands
+$(".footer-links li").hover(function(){
+	if($(this).hasClass("github")){
+		$(this).find(".fa").css("color", "#6cc644");
+	}
+	else if($(this).hasClass("codepen")){
+		$(this).find(".fa").css("color", "#FCD000");
+	}
+	else if($(this).hasClass("linkedin")){
+		$(this).find(".fa").css("color", "#0077b5");
+	}
+	else if($(this).hasClass("twitter")){
+		$(this).find(".fa").css("color", "#1DA1F2");
+	}
+	else if($(this).hasClass("instagram")){
+		$(this).find(".fa").css("color", "#833AB4");
+	}
+	else if($(this).hasClass("flickr")){
+		$(this).find(".fa").css("color", "#ff0084");
+	}
+	else if($(this).hasClass("blog")){
+		$(this).find(".fa").css("color", "#d54e21");
+	}
+}, 
+function(){
+	$(this).find(".fa").css("color", "#C15492");
+});
+
+})
