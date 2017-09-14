799fadca2fd8a7728e3eb118336a534e54cff304
Brittany Brassell <brittany.brassell@gmail.com>
2017-09-13 21:49:02 -0700

    skeleton for moving portfolio into blog family of styles. Needs to be organized! Everything's there except landing page. Need to add in content.

--- javascript/script.js
+++ javascript/script.js
@@ -7,6 +7,35 @@ $(document).ready(function(){
 		$(this).find(".excerptHover").removeClass("show");
 	});
 
+ // color hover on external links
+
+ // external link colors to match their site brands
+ $(".external-links li").hover(function(){
+ 	if($(this).hasClass("resume")){
+ 		$(this).find(".fa").css("color", "#fff");
+ 	}
+ 	else if($(this).hasClass("github")){
+ 		$(this).find(".fa").css("color", "#6cc644");
+ 	}
+ 	else if($(this).hasClass("codepen")){
+ 		$(this).find(".fa").css("color", "#FCD000");
+ 	}
+ 	else if($(this).hasClass("linkedin")){
+ 		$(this).find(".fa").css("color", "#0077b5");
+ 	}
+ 	else if($(this).hasClass("twitter")){
+ 		$(this).find(".fa").css("color", "#1DA1F2");
+ 	}
+ 	else if($(this).hasClass("instagram")){
+ 		$(this).find(".fa").css("color", "#833AB4");
+ 	}
+ 	else if($(this).hasClass("flickr")){
+ 		$(this).find(".fa").css("color", "#ff0084");
+ 	}
+ },
+ function(){
+ 	$(this).find(".fa").css("color", "#C15492");
+ });
 
 
 
