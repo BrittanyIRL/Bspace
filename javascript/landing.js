d1a6d464d88e23619f882b68caa86aed63a5a24e
Brittany Brassell <brittany.brassell@gmail.com>
2018-01-25 12:44:43 -0700

    more site landing

--- javascript/landing.js
+++ javascript/landing.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-  $(".hide_entry").css("display", "block");
+  $(".site_entry").css("display", "flex");
   $(".site_entry").find("#landing_two").delay(2000).addClass("fade_in");
 });
